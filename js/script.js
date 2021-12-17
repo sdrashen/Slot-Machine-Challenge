5c6ed20b591b6348c5dbb226c8925db45dce3cf9
Sabrina Drashen <sdrashen@gmail.com>
2021-12-17 12:49:10 -0300

    Added html, css, javascript and phaser

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const Phaser = require('phaser')
+const bar = 1
+const cherry = 2
+const lemon = 4
+const watermelon = 8
+const apple = 12
+const grape = 16
+const orange = 20
+const bana = 24
+
+window.onload = function() {
+    const game = new Phaser.Game(200, 200, Phaser.CANVAS)
+    const slots
+    const buttons
+
+}
